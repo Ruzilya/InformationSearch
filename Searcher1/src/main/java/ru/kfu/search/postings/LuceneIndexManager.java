5a6b499ece5e2e7c0a353e59dbb46f4d3e966540
Ruzilya <ruzilyasi@gmail.com>
2015-03-14 18:55:47 +0300

    Added new project for Task4

---
+++ Searcher1/src/main/java/ru/kfu/search/postings/LuceneIndexManager.java
@@ -0,0 +1,126 @@
+package ru.kfu.search.postings;
+
+import org.apache.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+* Created by RuzilyaS on 13-Mar-15.
+*/
+public class LuceneIndexManager {
+
+    private static Logger LOG = Logger.getLogger(LuceneIndexManager.class);
+
+    public static String docsDirPath;
+    public static String indexDirPath;
+
+    private static IndexSearcher searcher;
+    private static IndexReader reader;
+
+    public LuceneIndexManager(String docsDirPath, String indexPath){
+        this.docsDirPath=docsDirPath;
+        this.indexDirPath=indexPath;
+    }
+
+    public LuceneIndexManager(){
+        docsDirPath="src\\main\\resources\\filesToIndex";
+        indexDirPath="src\\main\\resources\\indexDirectory";
+    }
+
+    public static final String FIELD_CONTENTS = "contents";
+    public static final String FIELD_PATH = "path";
+    public static final String ENCODING = "UTF-8";
+
+    public void createIndex(){
+        final File docDir=new File(docsDirPath);
+        if (!docDir.exists() || !docDir.canRead()) {
+            LOG.info("Document directory '" + docDir.getAbsolutePath() + "' does not exist or is not readable, please check the path");
+            System.exit(1);
+        }
+        try {
+            LOG.info("Indexing to directory '" + indexDirPath + "'...");
+            Directory dir= FSDirectory.open(new File(indexDirPath));
+            Analyzer analyzer=new StandardAnalyzer(Version.LUCENE_31);
+            IndexWriterConfig iwc=new IndexWriterConfig(Version.LUCENE_31,analyzer);
+            iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+            IndexWriter writer=new IndexWriter(dir,iwc);
+            indexDocs(writer,docDir);
+            writer.close();
+        }
+        catch (IOException e) {
+            LOG.error("IOException while creating index.", e);
+        }
+    }
+
+    public IndexSearcher getSearcher() throws IOException {
+        if(searcher!=null){
+            return searcher;
+        }
+        reader = getReader();
+        searcher = new IndexSearcher(reader);
+        return searcher;
+    }
+
+    public IndexReader getReader() throws IOException {
+        if(reader!=null){
+            return reader;
+        }
+        File path = new File(indexDirPath);
+        Directory index = FSDirectory.open(path);
+        reader = DirectoryReader.open(index);
+        return reader;
+    }
+
+    public Terms getTerms() throws IOException {
+        Fields fields = MultiFields.getFields(getReader());
+        Terms terms = fields.terms(FIELD_CONTENTS);
+        return terms;
+    }
+
+    public List<Integer> getDocIds(String term)throws IOException{
+        TermQuery query = new TermQuery(new Term(FIELD_CONTENTS, term));
+        List<Integer> ids = new LinkedList<>();
+
+        int count = getReader().getDocCount(FIELD_CONTENTS);
+        TopDocs topdocs = getSearcher().search(query, count);
+        for ( ScoreDoc scoreDoc : topdocs.scoreDocs ) {
+//            Document doc = searcher.doc(scoreDoc.doc);
+            ids.add(scoreDoc.doc);
+        }
+        return ids;
+    }
+
+    private void indexDocs(IndexWriter writer,File file) throws IOException {
+        if (file.canRead()) {
+            if (file.isDirectory()) {
+                String[] files=file.list();
+                if (files != null) {
+                    for (int i=0; i < files.length; i++) {
+                        indexDocs(writer,new File(file,files[i]));
+                    }
+                }
+            }
+            else {
+                FileInputStream fis=new FileInputStream(file);
+                Document doc=new Document();
+                Field pathField=new Field(FIELD_PATH,file.getPath(),Field.Store.YES,Field.Index.NOT_ANALYZED_NO_NORMS);
+                doc.add(pathField);
+                doc.add(new Field(FIELD_CONTENTS,new BufferedReader(new InputStreamReader(fis,ENCODING))));
+                writer.addDocument(doc);
+                fis.close();
+            }
+        }
+    }
+
+}
---
+++ Searcher1/src/main/java/ru/kfu/search/postings/PostingsFile.java
@@ -0,0 +1,45 @@
+package ru.kfu.search.postings;
+
+import org.apache.lucene.index.Terms;
+import org.apache.lucene.index.TermsEnum;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.util.BytesRef;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by RuzilyaS on 14-Mar-15.
+ */
+public class PostingsFile {
+
+    private String postingsFilePath;
+    private String indexDirPath;
+
+    public PostingsFile(String path, String indexDirPath){
+        postingsFilePath=path;
+        this.indexDirPath = indexDirPath;
+    }
+
+    public File create() throws IOException, ParseException {
+        File file = new File(postingsFilePath);
+        LuceneIndexManager lim = new LuceneIndexManager();
+        lim.createIndex();
+        Terms terms =lim.getTerms();
+        TermsEnum iterator = terms.iterator(null);
+        BytesRef byteRef = null;
+        while((byteRef = iterator.next()) != null) {
+            String term = new String(byteRef.bytes, byteRef.offset, byteRef.length);
+            System.out.println(term);
+            System.out.println(lim.getDocIds(term));
+            System.out.println();
+        }
+
+        return file;
+    }
+
+    public static  void main(String args[]) throws IOException, ParseException {
+        PostingsFile pf = new PostingsFile("src\\main\\resources\\output", "src\\main\\resources\\indexDirectory");
+        pf.create();
+    }
+}
